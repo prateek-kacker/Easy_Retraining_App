6455bf31a94b6b140a903f0a19fc4b1f1bcd5050
Prateek Kacker <prateek.kacker@gmail.com>
2023-04-02 21:51:42 -0400

    Intermediate

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+'''
+This is a gradio app for uploading excel file, annotating the data, training the model and verifying the output for the model. You can keep repeating it till the model is getting desired results.
+'''
 import gradio as gr
 import pandas as pd
 
@@ -7,20 +10,57 @@ SENT2 = ''
 LABEL = ''
 TEXT_CATEGORIES= []
 MAX_TEXT_CATEGORIES=5
+MAX_LENGTH = 0
+DF_INDEX = 0
+NEXT = 1
+PREVIOUS = 0
 
+def moving_direction(direction):
+    '''
+    Function to move and back in the dataframe
+    '''
+    global MAX_LENGTH
+    global NEXT
+    global PREVIOUS
+    global DF_INDEX
+    if DF_INDEX == MAX_LENGTH-1 and direction == 'FORWARD':
+        NEXT = MAX_LENGTH-1
+        PREVIOUS = DF_INDEX-1
+        DF_INDEX = DF_INDEX
+    elif DF_INDEX == 0 and direction == 'BACKWARD':
+        PREVIOUS = 0
+        NEXT = DF_INDEX+1
+        DF_INDEX = DF_INDEX 
+    elif direction == 'FORWARD':
+        DF_INDEX = DF_INDEX+1
+        NEXT = DF_INDEX+1
+        PREVIOUS = DF_INDEX-1
+    elif direction == 'BACKWARD':
+        DF_INDEX = DF_INDEX-1
+        NEXT = DF_INDEX+1
+        PREVIOUS = DF_INDEX-1
+
+    
+    
+
+     
 def get_data_from_excel(excel_file):
     '''
     Function to load file when get_data_btn is executed
     '''
 
     global base_df
+    global MAX_LENGTH
     base_df = pd.read_excel(excel_file.name)
     base_df['Annoated']=False
     base_df['Predicted']=False
-
+    MAX_LENGTH = len(base_df)
     print(base_df)
     return base_df
 
+
+def verify_entire_predicted_fn():
+    pass
 
 def show_headers():
     '''
@@ -57,8 +97,27 @@ def len_of_not_annotated_fn():
 
 def annotate_data_fn():
     global base_df
+    global DF_INDEX
+    global NEXT
+    global PREVIOUS
     
+    text = base_df.iloc[DF_INDEX][SENT1]
+    unannotated_text= gr.update(text,visible=True)
+    category_radio=gr.update(visible=True)
+    previous_btn = gr.update(visible=True)
+    next_btn = gr.update(visible=True)
+    accept_btn = gr.update(visible=True)
+    return unannotated_text, category_radio, previous_btn, next_btn, accept_btn
+
     
+def train_fn():
+    pass    
+
+def annotate_predicted_fn():
+    pass
+
+def verify_entire_fn():
+    pass
 
 with gr.Blocks() as demo:
     gr.Markdown(
@@ -122,21 +181,42 @@ with gr.Blocks() as demo:
 
     with gr.Row() as row:
         unannotated_text= gr.Textbox(label='Text for annotation')
-        category_radio=gr.Radio(TEXT_CATEGORIES, label='Category',info='Select the appropriate category for the text')
+        category_radio=gr.Radio(TEXT_CATEGORIES,
+                                label='Category',
+                                info='Select the appropriate category for the text'
+                                )
     with gr.Row() as row:
             previous_btn = gr.Button('Previous')
             next_btn = gr.Button('Next')
     accept_btn = gr.Button('Accept the current category and move next')
-    annotate_unlabeled_btn.click(annotate_data_fn,inputs=[],outputs=[unannotated_text,category_radio,accept_btn])
+    annotate_unlabeled_btn.click(annotate_data_fn,
+                                 inputs=[],
+                                 outputs=[unannotated_text,
+                                          category_radio,
+                                          previous_btn,
+                                          next_btn,
+                                          accept_btn
+                                          ]
+                                )
 
     gr.Markdown(
         '''
         # Section 3 - Train the model based on past annotations
         '''
     )
-
-
-
+    epochs_text = gr.Textbox(value='5',
+                             label='Number of Epochs',
+                             interactive=True
+                             )
+    batch_text = gr.Textbox(value='32',
+                            label='Batch Size',
+                            interactive=True
+                            )
+    train_btn = gr.Button('Train Model')
+    train_end_text = gr.Textbox(value='Output data',
+                                label='Training End'
+                                )
+    train_btn.click(train_fn,inputs=[epochs_text,batch_text],outputs=[train_end_text])
 
 
 
@@ -145,6 +225,25 @@ with gr.Blocks() as demo:
         # Section 4 -Verify the output of the model on the data which is not annotated
         """
     )
+    with gr.Row() as row:
+        annotate_predicted_btn = gr.Button("Annotate Predicted Data")
+
+    with gr.Row() as row:
+        predicted_text= gr.Textbox(label='Text for confirmation on predicted data')
+        train_category_radio=gr.Radio(TEXT_CATEGORIES, label='Category',info='Appropriate category for the text as predicted by the model')
+    with gr.Row() as row:
+            train_previous_btn = gr.Button('Previous')
+            train_next_btn = gr.Button('Next')
+    train_accept_btn = gr.Button('Accept the category after verification and move to next')
+    annotate_predicted_btn.click(annotate_predicted_fn,
+                                 inputs=[],
+                                 outputs=[predicted_text,
+                                          train_category_radio,
+                                          train_previous_btn,
+                                          train_next_btn,
+                                          train_accept_btn
+                                          ]
+                                 )
 
 
     gr.Markdown(
@@ -152,9 +251,26 @@ with gr.Blocks() as demo:
         # Section 5 - Verify the entire annotated data once again
         """
     )
+
     with gr.Row() as row:
-        annotate_predicted_data_btn = gr.Button("Annotate predicted Data")
-        train_btn = gr.Button('Train Model')
+        verify_entire_btn = gr.Button("Verify Entire Data")
+
+    with gr.Row() as row:
+        predicted_entire_text= gr.Textbox(label='Text for confirmation on entire data')
+        predicted_entire_category_radio=gr.Radio(TEXT_CATEGORIES, label='Category',info='Appropriate category for the text as annotated by you')
+    with gr.Row() as row:
+            predict_entire_previous_btn = gr.Button('Previous')
+            predict_entire_next_btn = gr.Button('Next')
+    predict_entire_accept_btn = gr.Button('Accept the category after verification and move to next')
+    verify_entire_btn.click(verify_entire_predicted_fn,
+                            inputs=[],
+                            outputs=[predicted_entire_text,
+                                     predicted_entire_category_radio,
+                                     predict_entire_previous_btn,
+                                     predict_entire_next_btn,
+                                     predict_entire_accept_btn
+                                     ]
+                                     )
 
 
 demo.launch()
